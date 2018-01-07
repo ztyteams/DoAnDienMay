0e7ae9391754f6752113836694b4666a2422e60c
NhutZty <32875268+NhutZty@users.noreply.github.com>
2018-01-07 23:34:13 +0700

    new file updated
    fix lỗi,......

--- modules/mSearch.php
+++ modules/mSearch.php
@@ -1,8 +1,8 @@
 <div class="searchform">
-		
-		<form style="float:right;" action="index.php?a=15" method="post">
+		<form name="frmSearch" style="float:right;" action="index.php?a=15" method="post">
 			<span class="label1" >Tìm kiếm sản phẩm</span>
 			<input type="text" id="searchfor" name="searchtext" size="50"/>
-			<input type="submit" id="searchbtn" value="Search"/>
+			<input type="submit" id="searchbtn" value="Tìm kiếm"/>
+			<a href="index.php?a=16"><input type="button" id="searchadv" value="Tìm kiếm nâng cao"></a>
 		</form>
 </div>
--- pages/pSearch.php
+++ pages/pSearch.php
@@ -1,25 +1,35 @@
 <div>
 	<?php $search =$_POST["searchtext"];?> 
-	<span class="label1">Kết quả tìm kiếm của: <?php echo $search; ?> </span>
+	<span class="label2">Kết quả tìm kiếm từ: "<b style="font-size: 18px;color: #ff1a1a"><?php echo $search; ?></b>" </span>
 </div>	
 <?php
+		if($search=="" or $search==" ")
+		{
+?>
+			<b style="color: red;font-size: 32px"><?php echo "Không có thông tin từ sản phẩm.... :("; ?></b>
+<?php
+		}
+		else
+		{
         if(isset($_POST["searchtext"]))
-    {
-        $sql = "SELECT SanPham.MaSanPham, SanPham.TenSanPham, SanPham.GiaSanPham, SanPham.HinhURL, SanPham.SoLuongTon, LoaiSanPham.TenLoaiSanPham, HangSanXuat.TenHangSanXuat, SanPham.MoTa
-                FROM SanPham, LoaiSanPham, HangSanXuat
-                WHERE SanPham.TenSanPham like N'%".$search."%' and SanPham.BiXoa=FALSE";
-        $result = DataProvider::ExecuteQuery($sql);
-        while($row = mysqli_fetch_array($result))
-        {
-            $hinhURL = $row["HinhURL"];
-            $tenSanPham = $row["TenSanPham"];
-            $giaSanPham = $row["GiaSanPham"];
-            $tenHangSanXuat = $row["TenHangSanXuat"];
-            $tenLoaiSanPham = $row["TenLoaiSanPham"];
-            $soLuongTon = $row["SoLuongTon"];
-			$maSanPham =$row["MaSanPham"];
-            $moTa = $row["MoTa"];
-            include ("templates/tempThumbProduct.php");
-        }
-    }
+			{
+				$sql = "SELECT SanPham.MaSanPham, SanPham.TenSanPham, SanPham.GiaSanPham, SanPham.HinhURL, SanPham.SoLuongTon, LoaiSanPham.TenLoaiSanPham, HangSanXuat.TenHangSanXuat, SanPham.MoTa
+						FROM SanPham, LoaiSanPham, HangSanXuat
+						WHERE SanPham.TenSanPham LIKE N'%".$search."%' and SanPham.BiXoa=FALSE
+						GROUP BY SanPham.MaSanPham";
+				$result = DataProvider::ExecuteQuery($sql);
+				while($row = mysqli_fetch_array($result))
+				{
+					$hinhURL = $row["HinhURL"];
+					$tenSanPham = $row["TenSanPham"];
+					$giaSanPham = $row["GiaSanPham"];
+					$tenHangSanXuat = $row["TenHangSanXuat"];
+					$tenLoaiSanPham = $row["TenLoaiSanPham"];
+					$soLuongTon = $row["SoLuongTon"];
+					$maSanPham =$row["MaSanPham"];
+					$moTa = $row["MoTa"];
+					include ("templates/tempThumbProduct.php");
+				}
+			}
+		}
 ?>
