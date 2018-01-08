66ce96fef890493c3a48286f417068ae13835d4f
NhutZty <32875268+NhutZty@users.noreply.github.com>
2018-01-09 00:59:19 +0700

    thêm soluocxem

--- templates/tempChiTietSanPham.php
+++ templates/tempChiTietSanPham.php
@@ -23,18 +23,23 @@
             <span class="label">Số lượng tồn:</span>
             <span class="data"><?php echo $soLuongTon; ?> sản phẩm</span>
         </div>
+		<div>
+			<span class="label">Số lượt xem: </span>
+			<span class="data"><?php echo $soLuocXem; ?></span>
+		</div>
         <div class="giohang">
             <?php 
                 if(isset($_SESSION["maTaiKhoan"]))
                 {
                     ?>
                         <a href="index.php?a=12&id=<?php echo $maSanPham; ?>">
-                            <img src="img/shopping_cart.png" width="32">
+                            <img src="img/shopping_cart01.png" width="200">
                         </a>
                     <?php
                 }
             ?>
         </div>
+		
     </div>
     <div id="mota"><?php echo $moTa; ?></div>
 </div>
