cc0858b1dda542b55b3cd54af4663c0322740fbf
NhutZty <32875268+NhutZty@users.noreply.github.com>
2018-01-09 20:37:58 +0700

    new update
    .....

--- modules/mContent.php
+++ modules/mContent.php
@@ -33,23 +33,26 @@
 		case 14:
             include ("modules/mTrangDangKy.php");
             break;
-	case 15:
-	    include ("pages/pSearch.php");
-	    break;
-	case 16:
-	    include ("modules/mSearchAdv.php");
-	    break;
-	case 17:
-	    include ("pages/pSearchName.php");
-	    break; 
-	case 18:
-	    include ("pages/pSearchLoaiSP.php");
-	    break;
-	case 19:
-	  include ("pages/pSearchHSX.php");
-	  break;
+		case 15:
+			include ("pages/pSearch.php");
+			break;
+		case 16:
+			include ("modules/mSearchAdv.php");
+			break;
+		case 17:
+			include ("pages/pSearchName.php");
+			break;
+		case 18:
+			include ("pages/pSearchLoaiSP.php");
+			break;
+		case 19:
+			include ("pages/pSearchHSX.php");
+			break;
+		case 20:
+			include ("pages/pSearchGia.php");
+			break;
         default:
-          include "pages/pError.php";
-          break;
+            include "pages/pError.php";
+            break;
     }
 ?>
