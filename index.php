2625a5a7ef61a009cebdfcb3208709950609dafa
phong2906 <33740592+phong2906@users.noreply.github.com>
2018-01-09 14:06:49 +0700

    Repair

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		$_SESSION["curURL"] = $_SERVER["REQUEST_URI"];
 	
 	include_once ('lib/DataProvider.php'); 
-	include_once ('lib/ShoppingCart.php');
+	include_once ('lib/ZTYShop.php');
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
